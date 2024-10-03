f8e704ccfa9c7750801395ac9af32130ab4303e1
Muhammad Shafiq ur Rehman <shafikrehman@gmail.com>
2024-10-03 23:01:27 +0500

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ function calculateTip(){
     const totalAmount = tipAmount * billAmount;
 
     //Display the Result
-    document.getElementById("tip-amount").textContent = "$" + tipAmount;
-    document.getElementById("total-amount").textContent = "$" + totalAmount;
+    document.getElementById("tip-amount").textContent = "$" + tipAmount.toFixed(2);
+    document.getElementById("total-amount").textContent = "$" + totalAmount.toFixed(2);
 }
